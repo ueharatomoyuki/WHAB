ed4b54507535a456f4480a3be185994ecdae9993
uehara tomoyuki <22uehara_tomoyuki@axiz.co.jp>
2022-06-20 12:38:31 +0900

    header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 <?php
 $user= session('userInf');
 
@@ -6,11 +6,6 @@ $user= session('userInf');
 <header>
     <h3>
       ようこそ、{{ $user[0]->name }}さん
-=======
-<header>
-    <h3>
-      ようこそ、{{ $name }}さん
->>>>>>> 8f738281f47fb92688772dfe0836cd1d1084cb50
     </h3>
     <h2>{{ $heading }}</h2>
     <a href="logout" class="btn">ログアウト</a>
