8f738281f47fb92688772dfe0836cd1d1084cb50
miyagis <22miyagi_shiho@axiz.co.jp>
2022-06-20 11:40:08 +0900

    header,modalファイル

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,7 @@
+<header>
+    <h3>
+      ようこそ、{{ $name }}さん
+    </h3>
+    <h2>{{ $heading }}</h2>
+    <a href="logout" class="btn">ログアウト</a>
+</header>
---
+++ resources/views/components/modal.blade.php
@@ -0,0 +1,8 @@
+<div>
+    <div id="modal">
+        <p>{{ $mode }}してよろしいですか？</p>
+        <button type="submit" class="modal_btn">{{ $mode }}</button>
+        <button type="button" onclick="closeModal()" class="modal_btn">戻る</button>
+    </div>
+    <div id="fadeLayer"></div>
+</div>
